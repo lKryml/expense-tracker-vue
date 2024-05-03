9ffc2bbea0a0d27ec17259e26b853b20667ed38f
Krym <gumimash@gmail.com>
2024-05-03 14:33:45 +0200

    component structure + transaction history

---
+++ src/components/TransactionList.vue
@@ -0,0 +1,30 @@
+<template>
+  <h3>History</h3>
+  <ul id="list" class="list">
+    <li
+      v-for="tran in transactions"
+      v-bind:key="tran.id"
+      :class="tran.amount < 0 ? 'minus' : 'plus'"
+    >
+      {{ tran.text }} <span>{{ tran.amount }}</span
+      ><button class="delete-btn">x</button>
+    </li>
+    <li class="plus">
+      Paycheck <span>-$400</span><button class="delete-btn">x</button>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      transactions: [
+        { id: 1, text: "flower", amount: 100 },
+        { id: 2, text: "floweer", amount: 200 },
+        { id: 3, text: "flowaer", amount: -300 },
+      ],
+    };
+  },
+};
+</script>
